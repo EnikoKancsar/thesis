91c53f8157d056e32b56febc110bb9899b0dc828
EnikoKancsar <enikokancsar@gmail.com>
2022-03-22 16:13:48 +0100

    Prettify

--- unipose/transforms.py
+++ unipose/transforms.py
@@ -1,22 +1,12 @@
-# import collections
-# import cv2
-# import numbers
-# import numpy as np
-# import random
 import torch
 
 def to_tensor(pic):
-    """Convert a ``numpy.ndarray`` to tensor.
+    """Convert a numpy.ndarray to tensor.
 
     h , w , c -> c, h, w
+    :param pic (numpy.ndarray): Image to be converted to tensor.
 
-    See ``ToTensor`` for more details.
-
-    Args:
-        pic (numpy.ndarray): Image to be converted to tensor.
-
-    Returns:
-        Tensor: Converted image.
+    :returns: Tensor: Converted image.
     """
 
     img = torch.from_numpy(pic.transpose((2, 0, 1)))
